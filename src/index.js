1a2575cf54870c8e15ddf0c911e53bdb1e5a7301
Miguelanggelo Sumoza <miguelanggelo21@gmail.com>
2022-01-29 17:47:09 -0400

    mysql2
    modelo y rutas clientes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ const app = require("./app");
 const db = require("./db");
 
 const main = () => {
-  app.listen(app.get("port"), () => {
+  app.listen(app.get("port"), async () => {
     console.log(`Server listening on port ${app.get("port")}`);
-    console.log(`Connected to ${db.config.connectionConfig.database}`)
+    const con = await db.getConnection();
+    console.log(`Connected to ${con.config.database}`)
   })
 };
 
--- src/routes/clientes.js
+++ src/routes/clientes.js
@@ -4,9 +4,37 @@ const Cliente = require("../models/Cliente");
 
 const router = new Router();
 
-router.get("/",  async (req, res) => {
+// Obtener todos 
+router.get("/", async (req, res) => {
   const rows = await Cliente.findAll();
-  res.send(rows);
+  res.json(rows[0]);
+});
+
+// Obtener uno por su id
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const row = await Cliente.findById(id);
+  res.json(row[0]);
+});
+
+// Crear cliente
+router.post("/", async (req, res) => {
+  await Cliente.create(req.body);
+  res.status(201).json({ message: "Cliente creado" });
+});
+
+// Actualizar cliente
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  await Cliente.update(id, req.body);
+  res.json({ message: "Cliente actualizado" });
+});
+
+// Eliminar cliente
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  await Cliente.deleteCliente(id);
+  res.json({ message: `Cliente con id ${id} eliminado` });
 });
 
 module.exports = router;
